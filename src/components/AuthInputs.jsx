6828ec27ebfcabb54d362b9240ab21e176be9e92
kiwoonjung <kiwoon0627@gmail.com>
2024-08-14 01:16:02 -0700

    20240814 Pseudo Selectors, Nested Rules & Media Queries

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -15,18 +15,33 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: ${($invalid) => ($invalid ? "#F87171" : "#6B7280")};
+  color: ${({ $invalid }) => ($invalid ? "#F87171" : "#6B7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: ${($invalid) => ($invalid ? "#FED2D2" : "#D1D5DB")};
-  color: ${($invalid) => ($invalid ? "#EF4444" : "#374151")};
-  border: 1px solid ${($invalid) => ($invalid ? "#F73F3F" : "transparent")};
+  background-color: ${({ $invalid }) => ($invalid ? "#FED2D2" : "#D1D5DB")};
+  color: ${({ $invalid }) => ($invalid ? "#EF4444" : "#374151")};
+  border: 1px solid ${({ $invalid }) => ($invalid ? "#F73F3F" : "transparent")};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
 `;
 
 export default function AuthInputs() {
@@ -78,9 +93,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
